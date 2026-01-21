fdcbffc03ce2acc76953cba579d0535162b7b3a2
rutvabhatt130506-alt <rutva.bhatt130506@gmail.com>
2026-01-21 11:15:29 +0530

    Initial commit

---
+++ index2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Index2</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1>This is heading-1</h1>
+    <h2>This is heading-2</h2>
+    <h3>This is heading-3</h3>
+        <h4>This is heading-4</h4>
+    <h5>This is heading-5</h5>
+    <p>Paragraph</p>
+    
+        <p style="color: blueviolet
+  ;"> helloooooLorem ipsum dolor sit amet consectetur adipisicing elit. doloribus debitis quam accusantium saepe quaerat optio ea dolorem nulla
+        wsedrftvgbyhuuuuuuuuuuuuuuuuuusxrdcftvgbyhunjmknhbgfdsedrftvgybhnujmhnbgfdssedfvgbhnjmkwzsexdrcccccccccccccccccfbyhunjjjjjjjjjjjjjhbgvtfrderxcfvgbhnj</p>
+</body>
+
+</html>
